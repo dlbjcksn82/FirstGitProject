63d8c236f952f5daa909729e4a1c289975420f49
dlbjcksn82 <dj918@york.ac.uk>
2024-02-28 16:43:47 +0000

    Initial Commit

---
+++ FirstGitProject/FirstGitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstGitProjectApp.swift
+//  FirstGitProject
+//
+//  Created by Delbert Jackson on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstGitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
